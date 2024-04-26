a80da589e01e09659f2e6ff4e8739e23c1e2ef67
PP09PetyaDecryptor <166217226+PP09PetyaDecryptor@users.noreply.github.com>
2024-04-26 22:25:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const server = express();
 
 server.all(`/`, (req, res) => {
-    res.send(`Please connect me to a hosting website in-order to work 24/7.`);
+    res.send(`Please connect me to a cron-job.org website in-order to work 24/7.`);
 });
 
 function keepAlive() {
     server.listen(3000, () => {
-        console.log(`24/7 Activation Complete`);
+        console.log(`24/7 Activation Complete`)
+        console.log(`Logged in`)
     });
 }
 
