672a44fb62a8f8310f15b30b95c53a96f94df964
PP09PetyaDecryptor <166217226+PP09PetyaDecryptor@users.noreply.github.com>
2024-04-26 22:51:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,19 +24,19 @@ client.on('ready', async () => {
   console.log(`${client.user.tag} - rich presence started!`);
 
   const r = new Discord.RichPresence()
-    .setApplicationId('1195468879837810768') // make your bot in discord.com/developers and paste the application ID here
+    .setApplicationId('1233519365450895495') // make your bot in discord.com/developers and paste the application ID here
     .setType('PLAYING')
     .setURL('https://www.youtube.com/watch?v=P1Cu7ZE77BI') //Must be a youtube video link 
     .setState('LIFE IS ROBLOX!')
-    .setName('Roblox')
-    .setDetails(`LIFE IS ROBLOX!`) //[${formatTime()}] and this for showing time of stream.
+    .setName('Milan Debil')
+    .setDetails(`Subskrybuj Milan Debil!!`) //[${formatTime()}] and this for showing time of stream.
     .setStartTimestamp(Date.now())
     .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1231627991952396419/1233478432608747550/life_is_roblox.jpg?ex=662d3dcf&is=662bec4f&hm=f35ad93b8249e9bd8eb6ce6b1677c6e4327dd9a9ca2a91c829bd5f69f26b229f&') //You can put links in tenor or discord and etc. 
     .setAssetsLargeText('LIFE IS ROBLOX!') //Text when you hover the Large image
     .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1230237868707020982/1233486408132591626/image.png?ex=662d453c&is=662bf3bc&hm=0c17ca9e03aa46faa03009012c9ed37e37bb25356ff9e2afcf14607eb7b3a8c4&') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('LIFE IS ROBLOX') //Text when you hover the Small image
-    .addButton('LIFE IS ROBLOX!', 'https://roblox.com/')
-    .addButton('JOIN ROBOX', 'https://dsc.gg/roblox-i-gadanie-pl/');
+    .addButton('Milan Debil', 'https://youtube.com/@MilanDebil')
+    .addButton('GDL', 'https://youtube.com/@GeometryDashLivestreaming_/');
 
   client.user.setActivity(r);
   client.user.setPresence({ status: "idle" }); //dnd, online, idle, offline
